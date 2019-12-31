3d634495133dfe506b5cea116525751f77388c75
itsezc <33750251+itsezc@users.noreply.github.com>
2019-12-31 19:33:24 +0000

    Simple debug

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ app.use(express.static('public'));
 
 var io =socket(server);
 io.on('connection',function(socket){
+	
 	//console.log(socket.id);
+	
 	socket.on("chat",function(data){
+		console.log(data)
 		io.sockets.emit("chat",data);
 	});
+	
 	socket.on("typing",function(data){
 		socket.broadcast.emit("typing",data);
 	});
