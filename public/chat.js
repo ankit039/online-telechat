96fdd7acf7a1860707eed3845b1a35dde4412b22
ankit039 <ryder.raj.ankit@gmail.com>
2020-01-10 22:55:06 +0530

    final 9

--- public/chat.js
+++ public/chat.js
@@ -17,7 +17,7 @@ message.addEventListener("keypress",function(){
 socket.on("chat",function(data){
     feedback.innerHTML = "";
     output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
-    output.innerHTML=onclick="document.getElementById('message').value = ''";
+    onclick="document.getElementById('message').value = ''";
 });
 
 socket.on("typing",function(data){
